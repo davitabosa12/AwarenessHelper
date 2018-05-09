1dd85a9df8eb7e87b62ff1946bb242be77dbb2e7
Davi Batista Tabosa <376851@UFCVIRTUAL.LOCAL>
2018-05-09 15:37:30 -0300

    abstracao de HeadphoneFence

--- web/src/fence.js
+++ web/src/fence.js
@@ -1,5 +1,23 @@
-class Fence{}
+class Fence{
     constructor(fenceName){
-        this.fenceName = fenceName;
+        this._fenceName = fenceName;
     }
+    FenceTypes = Object.freeze({
+        BEACON : 0,
+        DETECTED_ACTIVITY : 1,
+        HEADPHONE : 2,
+        LOCATION : 3,
+        TIME : 4
+    });
+    setFenceType(fenceType){
+        if( fenceType == this.FenceTypes.BEACON || fenceType == this.FenceTypes.DETECTED_ACTIVITY || fenceType == this.FenceTypes.HEADPHONE ||
+            fenceType == this.FenceTypes.LOCATION || fenceType == this.FenceTypes.TIME)
+            this._fenceType = fenceType;
+        else
+            throw("Incompatible Fence Type");
+    }
+    setFenceMethod(){
+        
+    }
+    
 }
---
+++ web/src/headphoneFence.js
@@ -0,0 +1,55 @@
+class HeadphoneFence {
+    constructor(fenceName){
+        var _fenceName = fenceName;
+        this.getFenceName = function(){
+            return _fenceName;
+        }
+        this.setFenceName = function(newName){
+            _fenceName = newName;
+        }
+        
+    }
+    
+
+    /**
+     * 
+     * @param {String} methodName 
+     * @param {*} params 
+     */
+    setMethod(methodName, ...params){
+        var success = false;
+        if(methodName == "during"){
+            if(arguments.length <= 1){
+                throw("Invalid argument length for \"during\": expected 2, got " + arguments.length);
+            }
+            else if(!(arguments[1] == "pluggedIn" || arguments[1] == "unplugged")){
+                throw("Invalid argument for \"during\": parameter is not \"pluggedIn\" or \"unplugged\".");
+            }
+            else{
+                success = true;
+            }
+            
+        }
+
+        else if(methodName == "pluggingIn"){
+            success = true;
+        }
+
+        else if(methodName == "unplugging"){
+            
+            success = true;
+        }
+        if(success){
+            var _method = {
+                name: methodName,
+                state: arguments[1]
+            }
+            this.getMethod = function(){
+                return _method;
+            }
+        }
+        else
+            throw "Arguments not valid. First argument must be: \"during\", \"pluggingIn\" or \"unplugging\"";
+        
+    }
+}
